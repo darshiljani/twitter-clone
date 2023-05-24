05ee4a566bf216e82077d3e621401c7102de8cd4
Darshil Jani <68460069+darshiljani@users.noreply.github.com>
2023-05-24 10:26:19 +0530

    updated greeting times

--- src/components/Greetings/Greeting.tsx
+++ src/components/Greetings/Greeting.tsx
@@ -4,9 +4,9 @@ import EveningGreeting from "./EveningGreeting";
 
 const Greeting = () => {
   const time = new Date().getHours();
-  if (time < 10) {
+  if (time >=3 && time <= 12) {
     return <MorningGreeting />;
-  } else if (time < 20) {
+  } else if (time < 17) {
     return <AfternoonGreeting />;
   } else {
     return <EveningGreeting />;
